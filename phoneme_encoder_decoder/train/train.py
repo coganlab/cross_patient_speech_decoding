e32b8be7ec5b5e7185082a840803b5a70dbe3921
Zac Spalding <spaldingzac@gmail.com>
2023-03-17 13:10:54 -0400

    updated seq2seq training function to take in teacher forcing input and return predicted and true labels for each fold

--- phoneme_encoder_decoder/train/train.py
+++ phoneme_encoder_decoder/train/train.py
@@ -10,7 +10,8 @@ from sklearn.model_selection import StratifiedKFold
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras.models import load_model
 
-from processing_utils.sequence_processing import phoneme_padding, pre
+from processing_utils.sequence_processing import (predict_sequence,
+                                                  one_hot_decode_sequence)
 
 
 def shuffle_weights(model, weights=None):
@@ -37,14 +38,48 @@ def shuffle_weights(model, weights=None):
     model.set_weights(weights)
 
 
-def train_seq2seq_kfold(model, infenc, infdec, X, y, num_folds=10,
-                        batch_size=32, epochs=800, early_stop=True):
+def train_seq2seq_kfold(train_model, inf_enc, inf_dec, X, X_prior, y,
+                        num_folds=10, batch_size=32, epochs=800,
+                        early_stop=False):
+    """Trains a seq2seq encoder-decoder model using k-fold cross validation.
 
-    # save initial weights to reset models for each fold
-    init_train_w = model.get_weights()
+    Uses stratified k-fold cross validation to train a seq2seq encoder-decoder
+    model. Requires a training model, as well as inference encoder and decoder
+    for predicting sequences. Model is trained with teacher forcing from padded
+    versions of the target sequences. *** EARLY STOPPING IS CURRENTLY NOT
+    IMPLEMENTED ***.
 
-    # create padded sequences for teacher forcing
-    seq_prior, _, _, _ = phoneme_padding(y, 10)
+    Args:
+        train_model (Functional): Full encoder-decoder model for training.
+        inf_enc (Functional): Inference encoder model.
+        inf_dec (Functional): Inference decoder model.
+        X (ndarray): Feature data. First dimension should be number of
+            observations. Dimensions should be compatible with the input to the
+            provided models.
+        X_prior (ndarray): Shifted labels for teacher forcing. Dimensions
+            should be the same as `y`.
+        y (ndarray): Labels. First dimension should be number of observations.
+            Final dimension should be length of output sequence.
+        num_folds (int, optional): Number of CV folds. Defaults to 10.
+        batch_size (int, optional): Training batch size. Defaults to 32.
+        epochs (int, optional): Number of training epochs. Defaults to 800.
+        early_stop (bool, optional): Whether to stop training early based on
+            validation loss performance. Defaults to True.
+
+    Returns:
+        (Functional, Dict): Trained encoder-decoder model,
+            dictionary containing training performance history for each fold.
+            Dictionary structure is:
+                {'accuracy': [fold1_acc, fold2_acc, ...],
+                'loss': [fold1_loss, fold2_loss, ...],
+                'val_accuracy': [fold1_val_acc, fold2_val_acc, ...],
+                'val_loss': [fold1_val_loss, fold2_val_loss, ...]}
+    """
+    # save initial weights to reset model for each fold
+    init_train_w = train_model.get_weights()
+
+    n_output = inf_dec.output_shape[-1]  # number of output classes
+    seq_len = y.shape[-1]  # length of output sequence
 
     # define k-fold cross validation
     cv = StratifiedKFold(n_splits=num_folds, shuffle=True)
@@ -66,23 +101,29 @@ def train_seq2seq_kfold(model, infenc, infdec, X, y, num_folds=10,
     y_pred_all, y_test_all = [], []
     for train_ind, test_ind in cv.split(X, y):
         X_train, X_test = X[train_ind], X[test_ind]
-        X_prior_train, X_prior_test = seq_prior[train_ind], seq_prior[test_ind]
+        X_prior_train, X_prior_test = X_prior[train_ind], X_prior[test_ind]
         y_train, y_test = y[train_ind], y[test_ind]
 
         # reset model weights for current fold (also resets associated
         # inference weights)
-        shuffle_weights(model, weights=init_train_w)
+        shuffle_weights(train_model, weights=init_train_w)
 
-        history = model.fit([X_train, X_prior_train], y_train,
-                            batch_size=batch_size, epochs=epochs,
-                            validation_data=([X_test, X_prior_test], y_test),
-                            callbacks=cb)
-        saved_model = load_model('best_model.h5')
+        history = train_model.fit([X_train, X_prior_train], y_train,
+                                  batch_size=batch_size, epochs=epochs,
+                                  validation_data=([X_test, X_prior_test],
+                                                   y_test),
+                                  callbacks=cb)
         # TODO - figure out how to get inference weights from saved model
+        # saved_model = load_model('best_model.h5')
+
+        target = predict_sequence(inf_enc, inf_dec, X_test, X_prior_test,
+                                  seq_len, n_output)
+        y_test_all.append(one_hot_decode_sequence(y_test))
+        y_pred_all.append(one_hot_decode_sequence(target))
 
         histories['accuracy'].append(history.history['accuracy'])
         histories['loss'].append(history.history['loss'])
         histories['val_accuracy'].append(history.history['val_accuracy'])
         histories['val_loss'].append(history.history['val_loss'])
 
-    return model, histories
+    return train_model, histories
