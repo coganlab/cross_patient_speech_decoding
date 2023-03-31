1acd003f45fe8149fbce91cd8ed7b1b41c78f5df
Zac Spalding <spaldingzac@gmail.com>
2023-03-31 15:35:39 -0400

    moved dcc script outside of scripts folder

--- phoneme_encoder_decoder/train_rnn_gpu.py
+++ phoneme_encoder_decoder/train_rnn_gpu.py
@@ -5,13 +5,14 @@ Script to train a RNN model on the DCC cluster.
 from keras.optimizers import Adam
 from sklearn.metrics import balanced_accuracy_score
 
-from ..processing_utils.feature_data_from_mat import get_high_gamma_data
-from ..processing_utils.sequence_processing import pad_sequence_teacher_forcing
-from ..seq2seq_models.rnn_models import lstm_1Dcnn_model
-from ..train.train import train_seq2seq_kfold
-from ..visualization.plot_model_performance import plot_accuracy_loss
+from processing_utils.feature_data_from_mat import get_high_gamma_data
+from processing_utils.sequence_processing import pad_sequence_teacher_forcing
+from seq2seq_models.rnn_models import lstm_1Dcnn_model
+from train.train import train_seq2seq_kfold
+from visualization.plot_model_performance import plot_accuracy_loss
 
-DATA_PATH = '~/workspace/'
+# DATA_PATH = '~/workspace/'
+DATA_PATH = 'data/'
 
 # Load in data from workspace mat files
 hg_trace, hg_map, phon_labels = get_high_gamma_data(DATA_PATH +
@@ -34,9 +35,9 @@ train_model, inf_enc, inf_dec = lstm_1Dcnn_model(n_input_time, n_input_channel,
                                                  reg_lambda, bidir=bidir)
 
 # Train model
-num_folds = 10
+num_folds = 2
 batch_size = 200
-epochs = 200
+epochs = 1
 learning_rate = 5e-4
 
 train_model.compile(optimizer=Adam(learning_rate),
@@ -50,7 +51,7 @@ histories, y_pred_all, y_test_all = train_seq2seq_kfold(train_model, inf_enc,
 
 # Save outputs
 b_acc = balanced_accuracy_score(y_test_all, y_pred_all)
-with open(DATA_PATH + 'outputs/train1_b_acc.txt', 'w') as f:
+with open(DATA_PATH + 'outputs/train1_b_acc.txt', 'w+') as f:
     f.write(str(b_acc))
 
 plot_accuracy_loss(histories, epochs=epochs, save_fig=True,
