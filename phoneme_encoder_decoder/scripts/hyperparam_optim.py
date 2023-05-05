e00a9c8e71f71d2584d3544a1a81027e3f00eb65
zspald <spaldingzac@gmail.com>
2023-05-05 14:01:17 -0400

    created dcc script to run hyperparameter optimization with keras tuner

--- phoneme_encoder_decoder/scripts/hyperparam_optim.py
+++ phoneme_encoder_decoder/scripts/hyperparam_optim.py
@@ -4,10 +4,9 @@ Script to optimize hyperparameters for RNN model on the DCC cluster.
 
 import os
 import sys
+import argparse
 from contextlib import redirect_stdout
-from keras.optimizers import Adam
 import keras_tuner as kt
-from sklearn.metrics import balanced_accuracy_score
 from sklearn.model_selection import ShuffleSplit
 
 # too dumb to figure out python packages/importing between folders at the same
@@ -16,56 +15,115 @@ sys.path.insert(0, '..')
 
 from processing_utils.feature_data_from_mat import get_high_gamma_data
 from processing_utils.sequence_processing import pad_sequence_teacher_forcing
-from seq2seq_models.rnn_models import lstm_1Dcnn_model
 from train.optimize import encDecHyperModel, encDecTuner
-from visualization.plot_model_performance import plot_accuracy_loss
 
-HOME_PATH = os.path.expanduser('~')
-DATA_PATH = HOME_PATH + '/workspace/'
-# DATA_PATH = '../data/'
 
-# Load in data from workspace mat files
-hg_trace, hg_map, phon_labels = get_high_gamma_data(DATA_PATH +
-                                                    'S14/S14_HG_sigChannel'
-                                                    '.mat')
-n_output = 10
-X = hg_trace  # use HG traces (n_trials, n_channels, n_timepoints) for 1D CNN
-X_prior, y, _, _ = pad_sequence_teacher_forcing(phon_labels, n_output)
+def init_parser():
+    parser = argparse.ArgumentParser(description='Train RNN model on DCC')
+    parser.add_argument('-pt', '--patient', type=str, default='S14',
+                        required=False, help='Patient ID')
+    parser.add_argument('-s', '--sig_channels', type=str, default='True',
+                        required=False, help='Use significant channels (True)'
+                        'or all channels (False)')
+    parser.add_argument('-z', '--z_score', type=str, default='False',
+                        required=False, help='Z-score normalization (True)'
+                        'or mean-subtracted normalization (False)')
+    parser.add_argument('-v', '--verbose', type=int, default=1,
+                        required=False, help='Verbosity of model training')
+    parser.add_argument('-or', '--oracle', type=str, default='random',
+                        required=False, help='Random search (random) or '
+                        'Bayesian optimization (bayesian)')
+    parser.add_argument('-c', '--cluster', type=str, default='True',
+                        required=False,
+                        help='Run on cluster (True) or local (False)')
+    return parser
 
-n_input_time = X.shape[1]
-n_input_channel = X.shape[2]
 
-# test set definition for reporting
-test_size = 0.2
-data_split = ShuffleSplit(n_splits=1, test_size=test_size, random_state=2)
-train_idx, test_idx = next(data_split.split(X))
-X_train, X_test = X[train_idx], X[test_idx]
-X_prior_train, X_prior_test = X_prior[train_idx], X_prior[test_idx]
-y_train, y_test = y[train_idx], y[test_idx]
+def str2bool(s):
+    return s.lower() == 'true'
 
-# keras tuner optimization
-optim_trials = 5
-tuning_dir = 'rnn_tuning'
-project_dir = 'S14_1Dcnn_LSTM_v6'
-tuning_epochs = 800
-hyper_model = encDecHyperModel(lstm_1Dcnn_model, n_input_time, n_input_channel,
-                               n_output)
 
-# oracle = kt.oracles.RandomSearchOracle(
-#              objective=kt.Objective('val_accuracy', direction='max'),
-#              max_trials=optim_trials)
-oracle = kt.oracles.BayesianOptimizationOracle(
-             objective=kt.Objective('val_accuracy', direction='max'),
-             max_trials=optim_trials)
+def hyperparam_optim():
+    parser = init_parser()
+    args = parser.parse_args()
 
-rnn_optimizer = encDecTuner(hypermodel=hyper_model, oracle=oracle,
-                            directory=DATA_PATH + tuning_dir,
-                            project_name=project_dir)
-rnn_optimizer.search(X_train, X_prior_train, y_train, epochs=tuning_epochs)
+    inputs = {}
+    for key, val in vars(args).items():
+        inputs[key] = val
 
-# save trial data to text file
-summary_path = DATA_PATH + tuning_dir + '/' + project_dir +\
-               '/optim_summary.txt'
-with open(summary_path, 'w+') as f:
-    with redirect_stdout(f):
-        rnn_optimizer.results_summary()
+    pt = inputs['patient']
+    chan_ext = '_sigChannel' if str2bool(inputs['sig_channels']) else '_all'
+    norm_ext = '_zscore' if str2bool(inputs['z_score']) else ''
+    verbose = inputs['verbose']
+    cluster = str2bool(inputs['cluster'])
+    oracle_type = inputs['oracle']
+
+    if oracle_type not in ['random', 'bayesian']:
+        raise ValueError('Tuner oracle input must be "random" or "bayesian"')
+
+    if cluster:
+        HOME_PATH = os.path.expanduser('~')
+        DATA_PATH = HOME_PATH + '/workspace/'
+    else:
+        DATA_PATH = '../data/'
+
+    print('==================================================================')
+    print("Optimizing hyperparameters for patient %s." % pt)
+    print("Getting data from %s." % (DATA_PATH + f'{pt}/'))
+    print("Saving outputs to %s." % (DATA_PATH + 'rnn_tuning/'))
+    print('==================================================================')
+
+    # Load in data from workspace mat files
+    hg_trace, hg_map, phon_labels = get_high_gamma_data(DATA_PATH +
+                                                        f'{pt}/{pt}_HG'
+                                                        f'{chan_ext}'
+                                                        f'{norm_ext}'
+                                                        '.mat')
+
+    n_output = 10
+    X = hg_trace  # use HG traces (n_trials, n_channels, n_timepoints)
+    X_prior, y, _, _ = pad_sequence_teacher_forcing(phon_labels, n_output)
+
+    n_input_time = X.shape[1]
+    n_input_channel = X.shape[2]
+
+    # test set definition for reporting
+    test_size = 0.2
+    data_split = ShuffleSplit(n_splits=1, test_size=test_size, random_state=2)
+    train_idx, test_idx = next(data_split.split(X))
+    X_train, X_test = X[train_idx], X[test_idx]
+    X_prior_train, X_prior_test = X_prior[train_idx], X_prior[test_idx]
+    y_train, y_test = y[train_idx], y[test_idx]
+
+    # keras tuner optimization
+    max_optim_trials = 200
+    tuning_dir = 'rnn_tuning'
+    project_dir = 'S14_1Dcnn_EncDec'
+    tuning_epochs = 800
+    hyper_model = encDecHyperModel(n_input_time, n_input_channel, n_output)
+
+    if oracle_type == 'random':
+        oracle = kt.oracles.RandomSearchOracle(
+                    objective=kt.Objective('val_accuracy', direction='max'),
+                    max_trials=max_optim_trials)
+    elif oracle_type == 'bayesian':
+        oracle = kt.oracles.BayesianOptimizationOracle(
+                    objective=kt.Objective('seq2seq_val_accuracy',
+                                           direction='max'),
+                    max_trials=max_optim_trials)
+
+    rnn_optimizer = encDecTuner(hypermodel=hyper_model, oracle=oracle,
+                                directory=DATA_PATH + tuning_dir,
+                                project_name=project_dir)
+    rnn_optimizer.search(X_train, X_prior_train, y_train, epochs=tuning_epochs)
+
+    # save trial data to text file
+    summary_path = DATA_PATH + tuning_dir + '/' + project_dir +\
+        '/optim_summary.txt'
+    with open(summary_path, 'w+') as f:
+        with redirect_stdout(f):
+            rnn_optimizer.results_summary()
+
+
+if __name__ == '__main__':
+    hyperparam_optim()
