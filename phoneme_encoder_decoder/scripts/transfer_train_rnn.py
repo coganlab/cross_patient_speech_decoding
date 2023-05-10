7aff7b7e08fb69e3911c9df26431dbbacf3561b5
Zac Spalding <spaldingzac@gmail.com>
2023-05-10 11:16:48 -0400

    modified script to include kfold selection and combine history tracking across all transfer sessions

--- phoneme_encoder_decoder/scripts/transfer_train_rnn.py
+++ phoneme_encoder_decoder/scripts/transfer_train_rnn.py
@@ -15,8 +15,9 @@ sys.path.insert(0, '..')
 from processing_utils.feature_data_from_mat import get_high_gamma_data
 from processing_utils.sequence_processing import (pad_sequence_teacher_forcing,
                                                   decode_seq2seq)
-from seq2seq_models.rnn_models import lstm_1Dcnn_model
-from train.transfer_training import transfer_train_seq2seq_diff_chans
+from seq2seq_models.rnn_models import stacked_lstm_1Dcnn_model
+from train.transfer_training import (transfer_seq2seq_kfold_diff_chans,
+                                     transfer_train_seq2seq_diff_chans)
 from visualization.plot_model_performance import plot_accuracy_loss
 
 
@@ -34,6 +35,9 @@ def init_parser():
                         'or mean-subtracted normalization (False)')
     parser.add_argument('-n', '--num_iter', type=int, default=5,
                         required=False, help='Number of times to run model')
+    parser.add_argument('-k', '--k_fold', type=str, default='True',
+                        required=False, help='Perform k-fold CV (True)'
+                        'or not (False)')
     parser.add_argument('-v', '--verbose', type=int, default=1,
                         required=False, help='Verbosity of model training')
     parser.add_argument('-c', '--cluster', type=str, default='True',
@@ -63,6 +67,7 @@ def transfer_train_rnn():
     chan_ext = '_sigChannel' if str2bool(inputs['sig_channels']) else '_all'
     norm_ext = '_zscore' if str2bool(inputs['z_score']) else ''
     n_iter = inputs['num_iter']
+    kfold = str2bool(inputs['k_fold'])
     verbose = inputs['verbose']
     cluster = str2bool(inputs['cluster'])
 
@@ -112,8 +117,10 @@ def transfer_train_rnn():
     n_input_channel_trans = X2.shape[2]
     filter_size = 10
     n_filters = 100  # S14=100, S26=90
+    n_layers = 1
     n_units = 256  # S14=800, S26=900
     reg_lambda = 1e-6  # S14=1e-6, S26=1e-5
+    dropout = 0.33
     bidir = True
 
     # Train model
@@ -121,9 +128,13 @@ def transfer_train_rnn():
     num_folds = 10
     num_reps = 3
     batch_size = 200
-    epochs = 540
     learning_rate = 1e-3
 
+    pre_epochs = 200  # 200
+    conv_epochs = 60  # 60
+    ft_epochs = 540  # 540  
+    total_epochs = pre_epochs + conv_epochs + ft_epochs
+    
     # Hold out test data set
     data_split = ShuffleSplit(n_splits=1, test_size=test_size, random_state=2)
     train_idx, test_idx = next(data_split.split(X2))
@@ -136,19 +147,65 @@ def transfer_train_rnn():
         print('Iteration: ', i+1)
         print('==============================================================')
 
-        pre_model, pre_enc, pre_dec = lstm_1Dcnn_model(n_input_time,
-                                                       n_input_channel_pre,
-                                                       n_output, n_filters,
-                                                       filter_size, n_units,
-                                                       reg_lambda, bidir=bidir,
-                                                       dropout=0.33)
-        tar_model, tar_enc, tar_dec = lstm_1Dcnn_model(n_input_time,
-                                                       n_input_channel_trans,
-                                                       n_output, n_filters,
-                                                       filter_size, n_units,
-                                                       reg_lambda, bidir=bidir,
-                                                       dropout=0.33)
+        if kfold:
 
+            k_pre_model, k_pre_enc, k_pre_dec = stacked_lstm_1Dcnn_model(
+                                                    n_input_time,
+                                                    n_input_channel_pre,
+                                                    n_output, n_filters,
+                                                    filter_size, n_layers,
+                                                    n_units, reg_lambda,
+                                                    bidir=bidir,
+                                                    dropout=dropout)
+            k_tar_model, k_tar_enc, k_tar_dec = stacked_lstm_1Dcnn_model(
+                                                    n_input_time,
+                                                    n_input_channel_trans,
+                                                    n_output, n_filters,
+                                                    filter_size, n_layers,
+                                                    n_units, reg_lambda,
+                                                    bidir=bidir,
+                                                    dropout=dropout)
+
+            k_pre_model.compile(optimizer=Adam(learning_rate),
+                                loss='categorical_crossentropy',
+                                metrics=['accuracy'])
+            k_tar_model.compile(optimizer=Adam(learning_rate),
+                                loss='categorical_crossentropy',
+                                metrics=['accuracy'])
+
+            k_hist, y_pred_all, y_test_all = transfer_seq2seq_kfold_diff_chans(
+                                                k_pre_model, k_pre_enc,
+                                                k_pre_dec, k_tar_model,
+                                                k_tar_enc, k_tar_dec,
+                                                X1, X1_prior, y1, X2_train,
+                                                X2_prior_train, y2_train,
+                                                num_folds=num_folds,
+                                                num_reps=num_reps,
+                                                batch_size=batch_size,
+                                                pretrain_epochs=pre_epochs,
+                                                conv_epochs=conv_epochs,
+                                                fine_tune_epochs=ft_epochs,
+                                                verbose=verbose)
+
+            # final val acc - preds from inf decoder across all folds
+            val_acc = balanced_accuracy_score(y_test_all, y_pred_all)
+
+        # new models for training on all of training data (no validation split)
+        pre_model, pre_enc, pre_dec = stacked_lstm_1Dcnn_model(
+                                                n_input_time,
+                                                n_input_channel_pre,
+                                                n_output, n_filters,
+                                                filter_size, n_layers, n_units,
+                                                reg_lambda, bidir=bidir,
+                                                dropout=dropout)
+        tar_model, tar_enc, tar_dec = stacked_lstm_1Dcnn_model(
+                                            n_input_time,
+                                            n_input_channel_trans,
+                                            n_output, n_filters,
+                                            filter_size, n_layers, n_units,
+                                            reg_lambda, bidir=bidir,
+                                            dropout=dropout)
+        
         pre_model.compile(optimizer=Adam(learning_rate),
                           loss='categorical_crossentropy',
                           metrics=['accuracy'])
@@ -156,54 +213,66 @@ def transfer_train_rnn():
                           loss='categorical_crossentropy',
                           metrics=['accuracy'])
 
-        # t_hist, y_pred_all, y_test_all = transfer_seq2seq_kfold_diff_chans(
-        #                             pre_model, tar_model, tar_enc, tar_dec,
-        #                             X1, X1_prior, y1, X2_train, X2_prior_train,
-        #                             y2_train, num_folds=num_folds,
-        #                             batch_size=batch_size,
-        #                             fine_tune_epochs=epochs,
-        #                             num_reps=num_reps,
-        #                             verbose=verbose)
-
-        _, t_hist = transfer_train_seq2seq_diff_chans(pre_model, tar_model,
-                                                      X1, X1_prior, y1,
-                                                      X2_train,
-                                                      X2_prior_train,
-                                                      y2_train,
-                                                      fine_tune_epochs=epochs,
-                                                      verbose=verbose)
-
-        # # final val acc - preds from inf decoder across all folds
-        # val_acc = balanced_accuracy_score(y_test_all, y_pred_all)
+        _, t_hist = transfer_train_seq2seq_diff_chans(
+                            pre_model, tar_model,
+                            X1, X1_prior, y1,
+                            X2_train,
+                            X2_prior_train,
+                            y2_train,
+                            batch_size=batch_size,
+                            pretrain_epochs=pre_epochs,
+                            conv_epochs=conv_epochs,
+                            fine_tune_epochs=ft_epochs,
+                            verbose=verbose)
 
         # test acc
         y_pred_test, labels_test = decode_seq2seq(tar_enc, tar_dec, X2_test,
                                                   y2_test)
         test_acc = balanced_accuracy_score(labels_test, y_pred_test)
 
-        # with open(DATA_PATH + f'outputs/transfer_{pretrain_pt}-{transfer_pt}'
-        #           '_acc.txt', 'a+') as f:
-        #     f.write(f'Final validation accuracy: {val_acc}, '
-        #             f'Final test accuracy: {test_acc}' + '\n')
-        field_names = ['test_acc', 'labels_test', 'y_pred_test']
+        if kfold:
+            field_names = ['val_acc', 'test_acc', 'labels_test',
+                           'y_pred_test']
+        else:
+            field_names = ['test_acc', 'labels_test', 'y_pred_test']
+
         if inputs['out_filename'] != '':
             acc_filename = DATA_PATH + 'outputs/' + inputs['out_filename'] \
                            + '.csv'
         else:
-            acc_filename = DATA_PATH + ('outputs/transfer_'
-                                       f'{pretrain_pt}-{transfer_pt}'
-                                       f'{norm_ext}_acc.csv')
+            if kfold:
+                acc_filename = DATA_PATH + ('outputs/transfer_'
+                                            f'{pretrain_pt}-{transfer_pt}'
+                                            f'{norm_ext}_acc_kfold.csv')
+            else:
+                acc_filename = DATA_PATH + ('outputs/transfer_'
+                                            f'{pretrain_pt}-{transfer_pt}'
+                                            f'{norm_ext}_acc.csv')
+
         with open(acc_filename, 'a+', newline='') as f:
             writer = csv.DictWriter(f, fieldnames=field_names)
             # writer.writerow([test_acc] + labels_test + y_pred_test)
             # writer.writeheader()
-            writer.writerow({'test_acc': test_acc, 'labels_test': labels_test,
-                             'y_pred_test': y_pred_test})
+            if kfold:
+                writer.writerow({'val_acc': val_acc, 'test_acc': test_acc,
+                                 'labels_test': labels_test,
+                                 'y_pred_test': y_pred_test})
+            else:
+                writer.writerow({'test_acc': test_acc,
+                                 'labels_test': labels_test,
+                                 'y_pred_test': y_pred_test})
 
-        # plot_accuracy_loss(t_hist, epochs=epochs, save_fig=True,
-        #                    save_path=DATA_PATH +
-        #                    'outputs/plots/transfer_'
-        #                    f'{pretrain_pt}-{transfer_pt}_train_all_{i+1}.png')
+        # plot_tf_hist_loss_acc(t_hist, save_fig=True,
+        #                       save_path=DATA_PATH +
+        #                       (f'outputs/plots/transfer_{pretrain_pt}-'
+        #                         f'{transfer_pt}_train_{i+1}.png'))
+
+        if kfold:
+            plot_accuracy_loss(k_hist, epochs=total_epochs, save_fig=True,
+                               save_path=DATA_PATH +
+                               (f'outputs/plots/transfer_{pretrain_pt}-'
+                                f'{transfer_pt}_kfold_train_{i+1}.png'))
+
 
 
 if __name__ == '__main__':
