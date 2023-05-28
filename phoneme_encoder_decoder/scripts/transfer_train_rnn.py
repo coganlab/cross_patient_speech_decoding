7f0ca633b00ffad89eb942a5b0982075f27b9fc0
zspald <spaldingzac@gmail.com>
2023-05-28 12:28:40 -0400

    updated script to use current kfold eval method and pkl file saving

--- phoneme_encoder_decoder/scripts/transfer_train_rnn.py
+++ phoneme_encoder_decoder/scripts/transfer_train_rnn.py
@@ -4,22 +4,24 @@ Script to train a RNN model on the DCC.
 
 import os
 import sys
-import csv
 import argparse
+import numpy as np
 from keras.optimizers import Adam
+from sklearn.metrics import balanced_accuracy_score, confusion_matrix
 from sklearn.model_selection import ShuffleSplit
-from sklearn.metrics import balanced_accuracy_score
 
 sys.path.insert(0, '..')
 
 from processing_utils.feature_data_from_mat import get_high_gamma_data
 from processing_utils.sequence_processing import (pad_sequence_teacher_forcing,
                                                   decode_seq2seq)
+from processing_utils.data_saving import (append_pkl_accs, dict_from_lists,
+                                          save_pkl_params)
 from seq2seq_models.rnn_models import (stacked_lstm_1Dcnn_model,
                                        stacked_gru_1Dcnn_model)
 from train.transfer_training import (transfer_seq2seq_kfold_diff_chans,
                                      transfer_train_seq2seq_diff_chans)
-from visualization.plot_model_performance import plot_accuracy_loss
+from visualization.plot_model_performance import plot_loss_acc
 
 
 def init_parser():
@@ -37,16 +39,16 @@ def init_parser():
     parser.add_argument('-n', '--num_iter', type=int, default=5,
                         required=False, help='Number of times to run model')
     parser.add_argument('-k', '--k_fold', type=str, default='True',
-                        required=False, help='Perform k-fold CV (True)'
-                        'or not (False)')
+                        required=False, help='Evaluation via k-fold (True) or'
+                        'held-out test set (False)')
     parser.add_argument('-v', '--verbose', type=int, default=1,
                         required=False, help='Verbosity of model training')
     parser.add_argument('-c', '--cluster', type=str, default='True',
                         required=False,
                         help='Run on cluster (True) or local (False)')
-    parser.add_argument('-o', '--out_filename', type=str, default='',
+    parser.add_argument('-f', '--filename', type=str, default='',
                         required=False,
-                        help='Output filename for accuracy csv')
+                        help='Output filename for performance saving')
     return parser
 
 
@@ -133,83 +135,82 @@ def transfer_train_rnn():
     num_reps = 3
     batch_size = 200
     learning_rate = 1e-3
+    kfold_rand_state = 7
+
+    if inputs['filename'] != '':
+        acc_filename = DATA_PATH + 'outputs/' + inputs['filename'] + '.pkl'
+        plot_filename = DATA_PATH + ('outputs/plots/' + inputs['filename']
+                                     + '_train_%d.png')
+    else:
+        if kfold:
+            acc_filename = DATA_PATH + ('outputs/'
+                                        f'{pretrain_pt}->{transfer_pt}'
+                                        f'{norm_ext}_acc_'
+                                        f'{num_folds}fold.pkl')
+            plot_filename = DATA_PATH + ('outputs/'
+                                         f'{pretrain_pt}->{transfer_pt}'
+                                         f'{norm_ext}'
+                                         f'{num_folds}fold_train_%d.png')
+        else:
+            acc_filename = DATA_PATH + ('outputs/'
+                                        f'{pretrain_pt}->{transfer_pt}'
+                                        f'{norm_ext}_acc_'
+                                        f'{test_size}-heldout.pkl')
+            plot_filename = DATA_PATH + ('outputs/'
+                                         f'{pretrain_pt}->{transfer_pt}'
+                                         f'{norm_ext}'
+                                         f'{test_size}-heldout_train_%d.png')
 
     pre_epochs = 200  # 200
     conv_epochs = 60  # 60
-    ft_epochs = 540  # 540  
+    ft_epochs = 540  # 540
     total_epochs = pre_epochs + conv_epochs + ft_epochs
-    
-    # Hold out test data set
-    data_split = ShuffleSplit(n_splits=1, test_size=test_size, random_state=2)
-    train_idx, test_idx = next(data_split.split(X2))
-    X2_train, X2_test = X2[train_idx], X2[test_idx]
-    X2_prior_train, X2_prior_test = X2_prior[train_idx], X2_prior[test_idx]
-    y2_train, y2_test = y2[train_idx], y2[test_idx]
+
+    param_keys = ['model_type', 'filter_size', 'n_filters', 'n_units',
+                  'n_layers', 'reg_lambda', 'dropout', 'bidir',
+                  'num_folds', 'num_reps', 'pre_epochs', 'conv_epochs',
+                  'target_epochs', 'learning_rate', 'kfold_rand_state']
+    param_vals = [model_type, filter_size, n_filters, n_units,
+                  n_layers, reg_lambda, dropout, bidir,
+                  num_folds, num_reps, pre_epochs, conv_epochs,
+                  ft_epochs, learning_rate, kfold_rand_state]
+    save_pkl_params(acc_filename, dict_from_lists(param_keys, param_vals))
+
+    if not kfold:
+        # Hold out test data set
+        data_split = ShuffleSplit(n_splits=1, test_size=test_size,
+                                  random_state=2)
+        train_idx, test_idx = next(data_split.split(X2))
+        X2_train, X2_test = X2[train_idx], X2[test_idx]
+        X2_prior_train, X2_prior_test = X2_prior[train_idx], X2_prior[test_idx]
+        y2_train, y2_test = y2[train_idx], y2[test_idx]
+    else:
+        X2_train = X2
+        X2_prior_train = X2_prior
+        y2_train = y2
 
     for i in range(n_iter):
         print('==============================================================')
         print('Iteration: ', i+1)
         print('==============================================================')
 
-        if kfold:
-
-            k_pre_model, k_pre_enc, k_pre_dec = model_fcn(
-                                                    n_input_time,
-                                                    n_input_channel_pre,
-                                                    n_output, n_filters,
-                                                    filter_size, n_layers,
-                                                    n_units, reg_lambda,
-                                                    bidir=bidir,
-                                                    dropout=dropout)
-            k_tar_model, k_tar_enc, k_tar_dec = model_fcn(
-                                                    n_input_time,
-                                                    n_input_channel_trans,
-                                                    n_output, n_filters,
-                                                    filter_size, n_layers,
-                                                    n_units, reg_lambda,
-                                                    bidir=bidir,
-                                                    dropout=dropout)
-
-            k_pre_model.compile(optimizer=Adam(learning_rate),
-                                loss='categorical_crossentropy',
-                                metrics=['accuracy'])
-            k_tar_model.compile(optimizer=Adam(learning_rate),
-                                loss='categorical_crossentropy',
-                                metrics=['accuracy'])
-
-            k_hist, y_pred_all, y_test_all = transfer_seq2seq_kfold_diff_chans(
-                                                k_pre_model, k_pre_enc,
-                                                k_pre_dec, k_tar_model,
-                                                k_tar_enc, k_tar_dec,
-                                                X1, X1_prior, y1, X2_train,
-                                                X2_prior_train, y2_train,
-                                                num_folds=num_folds,
-                                                num_reps=num_reps,
-                                                batch_size=batch_size,
-                                                pretrain_epochs=pre_epochs,
-                                                conv_epochs=conv_epochs,
-                                                fine_tune_epochs=ft_epochs,
-                                                verbose=verbose)
-
-            # final val acc - preds from inf decoder across all folds
-            val_acc = balanced_accuracy_score(y_test_all, y_pred_all)
-
-        # new models for training on all of training data (no validation split)
         pre_model, pre_enc, pre_dec = model_fcn(
-                                            n_input_time,
-                                            n_input_channel_pre,
-                                            n_output, n_filters,
-                                            filter_size, n_layers, n_units,
-                                            reg_lambda, bidir=bidir,
-                                            dropout=dropout)
+                                                n_input_time,
+                                                n_input_channel_pre,
+                                                n_output, n_filters,
+                                                filter_size, n_layers,
+                                                n_units, reg_lambda,
+                                                bidir=bidir,
+                                                dropout=dropout)
         tar_model, tar_enc, tar_dec = model_fcn(
-                                            n_input_time,
-                                            n_input_channel_trans,
-                                            n_output, n_filters,
-                                            filter_size, n_layers, n_units,
-                                            reg_lambda, bidir=bidir,
-                                            dropout=dropout)
-        
+                                                n_input_time,
+                                                n_input_channel_trans,
+                                                n_output, n_filters,
+                                                filter_size, n_layers,
+                                                n_units, reg_lambda,
+                                                bidir=bidir,
+                                                dropout=dropout)
+
         pre_model.compile(optimizer=Adam(learning_rate),
                           loss='categorical_crossentropy',
                           metrics=['accuracy'])
@@ -217,66 +218,52 @@ def transfer_train_rnn():
                           loss='categorical_crossentropy',
                           metrics=['accuracy'])
 
-        _, t_hist = transfer_train_seq2seq_diff_chans(
-                            pre_model, tar_model,
-                            X1, X1_prior, y1,
-                            X2_train,
-                            X2_prior_train,
-                            y2_train,
-                            batch_size=batch_size,
-                            pretrain_epochs=pre_epochs,
-                            conv_epochs=conv_epochs,
-                            fine_tune_epochs=ft_epochs,
-                            verbose=verbose)
-
-        # test acc
-        y_pred_test, labels_test = decode_seq2seq(tar_enc, tar_dec, X2_test,
-                                                  y2_test)
-        test_acc = balanced_accuracy_score(labels_test, y_pred_test)
-
         if kfold:
-            field_names = ['val_acc', 'test_acc', 'labels_test',
-                           'y_pred_test']
+            k_hist, y_pred_all, y_test_all = transfer_seq2seq_kfold_diff_chans(
+                                                pre_model, pre_enc,
+                                                pre_dec, tar_model,
+                                                tar_enc, tar_dec,
+                                                X1, X1_prior, y1, X2_train,
+                                                X2_prior_train, y2_train,
+                                                num_folds=num_folds,
+                                                num_reps=num_reps,
+                                                rand_state=kfold_rand_state,
+                                                batch_size=batch_size,
+                                                pretrain_epochs=pre_epochs,
+                                                conv_epochs=conv_epochs,
+                                                fine_tune_epochs=ft_epochs,
+                                                verbose=verbose)
+
+            # final val acc - preds from inf decoder across all folds
+            acc = balanced_accuracy_score(y_test_all, y_pred_all)
+            cmat = confusion_matrix(y_test_all, y_pred_all,
+                                    labels=range(1, n_output))
+
+            plot_loss_acc(k_hist, epochs=total_epochs, save_fig=True,
+                          save_path=plot_filename % (i+1))
         else:
-            field_names = ['test_acc', 'labels_test', 'y_pred_test']
+            _, _ = transfer_train_seq2seq_diff_chans(
+                                pre_model, tar_model,
+                                X1, X1_prior, y1,
+                                X2_train,
+                                X2_prior_train,
+                                y2_train,
+                                batch_size=batch_size,
+                                pretrain_epochs=pre_epochs,
+                                conv_epochs=conv_epochs,
+                                fine_tune_epochs=ft_epochs,
+                                verbose=verbose)
 
-        if inputs['out_filename'] != '':
-            acc_filename = DATA_PATH + 'outputs/' + inputs['out_filename'] \
-                           + '.csv'
-        else:
-            if kfold:
-                acc_filename = DATA_PATH + ('outputs/transfer_'
-                                            f'{pretrain_pt}-{transfer_pt}'
-                                            f'{norm_ext}_acc_kfold.csv')
-            else:
-                acc_filename = DATA_PATH + ('outputs/transfer_'
-                                            f'{pretrain_pt}-{transfer_pt}'
-                                            f'{norm_ext}_acc.csv')
+            # test acc
+            y_pred_test, labels_test = decode_seq2seq(tar_enc, tar_dec,
+                                                      X2_test, y2_test)
+            acc = balanced_accuracy_score(labels_test, y_pred_test)
+            cmat = confusion_matrix(labels_test, y_pred_test,
+                                    labels=range(1, n_output))
 
-        with open(acc_filename, 'a+', newline='') as f:
-            writer = csv.DictWriter(f, fieldnames=field_names)
-            # writer.writerow([test_acc] + labels_test + y_pred_test)
-            # writer.writeheader()
-            if kfold:
-                writer.writerow({'val_acc': val_acc, 'test_acc': test_acc,
-                                 'labels_test': labels_test,
-                                 'y_pred_test': y_pred_test})
-            else:
-                writer.writerow({'test_acc': test_acc,
-                                 'labels_test': labels_test,
-                                 'y_pred_test': y_pred_test})
-
-        # plot_tf_hist_loss_acc(t_hist, save_fig=True,
-        #                       save_path=DATA_PATH +
-        #                       (f'outputs/plots/transfer_{pretrain_pt}-'
-        #                         f'{transfer_pt}_train_{i+1}.png'))
-
-        if kfold:
-            plot_accuracy_loss(k_hist, epochs=total_epochs, save_fig=True,
-                               save_path=DATA_PATH +
-                               (f'outputs/plots/transfer_{pretrain_pt}-'
-                                f'{transfer_pt}_kfold_train_{i+1}.png'))
-
+        # save performance
+        append_pkl_accs(acc_filename, acc, cmat, acc_key='val_acc' if kfold
+                        else 'test_acc')
 
 
 if __name__ == '__main__':
