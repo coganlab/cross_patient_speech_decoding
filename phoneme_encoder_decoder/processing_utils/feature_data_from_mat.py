576ba9be55ad2e59bef28858cafba335e2f27fbf
Zac Spalding <spaldingzac@gmail.com>
2023-07-14 15:35:02 -0400

    added p1, p2, p3 centered data with associated loading fcns

--- phoneme_encoder_decoder/processing_utils/feature_data_from_mat.py
+++ phoneme_encoder_decoder/processing_utils/feature_data_from_mat.py
@@ -8,6 +8,26 @@ import numpy as np
 import scipy.io as sio
 
 
+def load_subject_high_gamma(subject_id, sig_channel=False, zscore=False):
+
+    filename = process_mat_filename(subject_id, sig_channel, zscore)
+    hg_trace, hg_map, phon_labels = get_high_gamma_data(filename)
+
+    return hg_trace, hg_map, phon_labels
+
+
+def load_subject_high_gamma_phoneme(subject_id, phons=[1, 2, 3]):
+    subj_dict = dict(ID=subject_id)
+    for p in phons:
+        filename = process_mat_filename(subject_id, True, False, phon=p)
+        hg_trace, hg_map, phon_labels = get_high_gamma_data(filename)
+        subj_dict['X' + str(p)] = hg_trace
+        subj_dict['X' + str(p) + '_map'] = hg_map
+        subj_dict['y' + str(p)] = phon_labels[:, p-1]
+    subj_dict['y_full'] = phon_labels
+    return subj_dict
+
+
 def load_mat_data(filename):
     return sio.loadmat(filename)
 
@@ -16,7 +36,7 @@ def get_feature_data(mat_data, feature_name):
     return np.squeeze(np.array(mat_data[feature_name]))
 
 
-def process_mat_filename(subject_id, sig_channel, zscore):
+def process_mat_filename(subject_id, sig_channel, zscore, phon=None):
     data_dir = 'data/'
     subject_dir = subject_id + '/'
     filename_base = subject_id + '_HG'
@@ -28,9 +48,13 @@ def process_mat_filename(subject_id, sig_channel, zscore):
         norm_suffix = '_zscore'
     else:
         norm_suffix = ''
+    if phon is not None:
+        phon_suffix = '_p' + str(phon)
+    else:
+        phon_suffix = ''
 
-    filename = str(data_dir + subject_dir + filename_base + chan_suffix +
-                   norm_suffix + '_goodTrials.mat')
+    filename = str(data_dir + subject_dir + filename_base + phon_suffix +
+                   chan_suffix + norm_suffix + '_goodTrials.mat')
     return filename
 
 
@@ -44,13 +68,5 @@ def get_high_gamma_data(filename):
     hg_map = get_feature_data(mat_data, 'hgMap')
     # shape = trials x 3 (3 length phoneme sequence)
     phon_labels = get_feature_data(mat_data, 'phonSeqLabels')
-
-    return hg_trace, hg_map, phon_labels
-
-
-def load_subject_high_gamma(subject_id, sig_channel=False, zscore=False):
-
-    filename = process_mat_filename(subject_id, sig_channel, zscore)
-    hg_trace, hg_map, phon_labels = get_high_gamma_data(filename)
 
     return hg_trace, hg_map, phon_labels
