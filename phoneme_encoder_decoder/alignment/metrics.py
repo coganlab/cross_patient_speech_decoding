3ba5affdf2cf97a5e106cd83d2674a4f2d139fe8
Zac Spalding <spaldingzac@gmail.com>
2023-08-23 12:59:25 -0400

    file setup for future alignment comparison analyses

---
+++ phoneme_encoder_decoder/alignment/metrics.py
@@ -0,0 +1,12 @@
+""" Metrics for comparing quality of different cross-patient dataset alignment
+methods.
+
+Author: Zac Spalding
+Cogan & Viventi Labs, Duke University
+"""
+
+import numpy as np
+
+
+def pt_corr():
+    pass
