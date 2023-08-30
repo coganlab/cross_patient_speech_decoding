9bae24ae01dabcf3273a35054893688b7192cd99
Zac Spalding <spaldingzac@gmail.com>
2023-08-30 15:42:08 -0400

    created class for joint PCA decomp that implements fit and transform methods

--- phoneme_encoder_decoder/alignment/alignment_methods.py
+++ phoneme_encoder_decoder/alignment/alignment_methods.py
@@ -9,27 +9,143 @@ from sklearn.decomposition import PCA
 from utils import cnd_avg, label2str
 
 
-def apply_joint_PCA_decomp(features, labels, n_components=40, dim_red=PCA):
-    """Wrapper function around joint PCA decomposition to calculate shared
-    latent space and also transform input
+class JointPCADecomp:
 
-    Args:
-        features (_type_): _description_
-        labels (_type_): _description_
-        n_components (int, optional): _description_. Defaults to 40.
-        dim_red (_type_, optional): _description_. Defaults to PCA.
+    def __init__(self, n_components=40, dim_red=PCA):
+        """Initializes JointPCADecomp class with the number of latent
+        components and the method for dimensionality reduction.
 
-    Returns:
-        _type_: _description_
-    """
-    latent_transforms = get_joint_PCA_transforms(features, labels,
-                                                 n_components=n_components,
-                                                 dim_red=dim_red)
-    transform_feats_lst = [0]*len(latent_transforms)
-    for i, (feats, transform) in enumerate(zip(features, latent_transforms)):
-        transform_feats = feats.reshape(-1, feats.shape[-1]) @ transform
-        transform_feats_lst[i] = transform_feats.reshape(feats.shape[0], -1)
-    return (*transform_feats_lst,)
+        Args:
+            n_components (int, optional): Number of components for
+                dimensionality reduction i.e. dimensionality of latent space.
+                Defaults to 40.
+            dim_red (Callable, optional): Dimensionality reduction function.
+                Must implement sklearn-style fit_transform() function. Defaults
+                to PCA.
+        """
+        self.n_components = n_components
+        self.dim_red = dim_red
+
+    def fit(self, X, y):
+        """Learns source-specific (e.g. patient-specific) transformations to
+        the shared latent space and stores transformations in self.transforms.
+
+        Args:
+            X (list of ndarray): List of features from multiple sources to
+            compute shared latent space.
+            y (list of ndarray): List of labels corresponding to feature
+            sources. Must be the same length as features.
+        """
+        transforms = get_joint_PCA_transforms(X, y,
+                                              n_components=self.n_components,
+                                              dim_red=self.dim_red)
+        self.transforms = transforms
+
+    def transform(self, X, idx=-1):
+        """Applies learned transformations to input data. Supports transforming
+        a single, specified dataset or all-source datasets at once.
+
+        Args:
+            X (ndarray or list of ndarray): Features to transform. If a list,
+                the length must be equal to the number of learned transforms
+                (i.e. transforming all sources). If an ndarray, a
+                source-specific transformation is applied to the data, with the
+                source specified by the idx input.
+            idx (int, optional): Index of saved transform list to apply to
+                single source data, or -1 if applying to all sources. Defaults
+                to -1.
+
+        Raises:
+            IndexError: Error if idx is too large to select a learned transform
+                from the saved list.
+            RuntimeError: Error if fit() has not been called before calling
+                transform().
+
+        Returns:
+            ndarray or tuple: Transformed data from single session if idx is
+                not -1, or a tuple of containing:
+                    Transformed data from all sources input to the fit()
+                    method. Length will be equal to the number of learned
+                    transformations.
+        """
+        if not self._check_fit():
+            raise RuntimeError('Must fit() before transforming data.')
+        if idx == -1:
+            return self._transform_multiple(X)
+        if idx >= len(self.transforms):
+            raise IndexError('Input idx is greater than the number of learned '
+                             'transforms. For transformation of data from a '
+                             'specific session, provide the input idx as the '
+                             'index of the session in the input list. If '
+                             'transforming multiple sessions, set idx=-1 '
+                             '(default).')
+        return self._transform_single(X, idx)
+
+    def fit_transform(self, X, y):
+        """Fits the model with X and y and applies the learned transformations
+        to X.
+
+        Args:
+            X (list of ndarray): List of features from multiple sources to
+            compute shared latent space.
+            y (list of ndarray): List of labels corresponding to feature
+            sources. Must be the same length as features.
+
+        Returns:
+            tuple: tuple of containing:
+                    Transformed ndarray data from all sources input to the
+                    fit() method. Length will be equal to the number of learned
+                    transformations.
+        """
+        self.fit(X, y)
+        return self.transform(X)
+
+    def _transform_multiple(self, X):
+        """Uses learned latent space transformations to transform data from all
+        sources used to fit the decomposition.
+
+        Args:
+            X (list of ndarray): List of features from multiple sources to
+            compute shared latent space.
+
+        Returns:
+            tuple of ndarray: tuple of containing:
+                    Transformed ndarray data from all sources input to the
+                    fit() method. Length will be equal to the number of learned
+                    transformations.
+        """
+        transformed_lst = [0]*len(X)
+        for i, (feats, transform) in enumerate(zip(X, self.transforms)):
+            transform_feats = feats.reshape(-1, feats.shape[-1]) @ transform
+            transformed_lst[i] = transform_feats.reshape(X.shape[:-1] + (-1,))
+        return (*transformed_lst,)
+
+    def _transform_single(self, X, idx):
+        """Applies learned latent space transformations to data from a single
+        a single source specified by idx.
+
+        Args:
+            X (ndarray): Features to transform.
+            idx (int): Index of learned transforms to apply to X.
+
+        Returns:
+            ndarray: Features transformed to shared latent space.
+        """
+        transform = self.transforms[idx]
+        transform_feats = X.reshape(-1, X.shape[-1]) @ transform
+        return transform_feats.reshape(X.shape[:-1] + (-1,))
+
+    def _check_fit(self):
+        """Checks if the joint PCA decomposition has been fit to data.
+
+        Returns:
+            boolean: True if fit() has been called, False otherwise.
+        """
+        try:
+            self.transforms
+        except AttributeError:
+            return False
+        return True
 
 
 def get_joint_PCA_transforms(features, labels, n_components=40, dim_red=PCA):
@@ -52,7 +168,7 @@ def get_joint_PCA_transforms(features, labels, n_components=40, dim_red=PCA):
             implement sklearn-style fit_transform() function. Defaults to PCA.
 
     Returns:
-        (tuple): tuple containing:
+        tuple: tuple containing:
             Transformation matrices to shared latent space for each input
             source. Length will be equal to the length of the input feature
             list.
@@ -108,7 +224,7 @@ def CCA_align_by_class(X_a, X_b, y_a, y_b, return_space='b_to_a'):
             ('shared'). Defaults to 'b_to_a'.
 
     Returns:
-        (tuple): tuple containing:
+        tuple: tuple containing:
             X_a (ndarray): Aligned data matrix for dataset A
             X_b (ndarray): Aligned data matrix for dataset B
     """
@@ -171,7 +287,7 @@ def CCA_align_by_trial_subselect(X_a, X_b, y_a, y_b, return_space='b_to_a'):
             ('shared'). Defaults to 'b_to_a'.
 
     Returns:
-        (tuple): tuple containing:
+        tuple: tuple containing:
             X_a (ndarray): Aligned data matrix for dataset A
             X_b (ndarray): Aligned data matrix for dataset B
     """
@@ -242,7 +358,7 @@ def CCA_align(L_a, L_b):
         L_b (ndarray): Latent dynamics array for dataset B of shape (m, T)
 
     Returns:
-        (tuple): tuple containing:
+        tuple: tuple containing:
             M_a (ndarray): Manifold directions for dataset A of shape (m, m)
             M_b (ndarray): Manifold directions for dataset B of shape (m, m)
     """
