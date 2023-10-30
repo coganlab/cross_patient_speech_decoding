e82472f1187f7e06e0a301f64dc3c1b007d18c8d
zspald <spaldingzac@gmail.com>
2023-10-30 14:13:11 -0400

    implemented cross-pt decoder class compatible with sklearn cv searches

---
+++ phoneme_encoder_decoder/alignment/cross_pt_decoders.py
@@ -0,0 +1,61 @@
+""" Cross-Patient Decoder Classes
+
+Author: Zac Spalding
+"""
+
+import numpy as np
+from sklearn.base import BaseEstimator
+from sklearn.decomposition import PCA
+
+class crossPtDecoder_sepDimRed(BaseEstimator):
+    """ Cross-Patient Decoder with separate PCA for each patient. """
+    
+    def __init__(self, cross_pt_data, decoder, dim_red=PCA, n_comp=10):
+        self.cross_pt_data = cross_pt_data
+        self.decoder = decoder
+        self.dim_red = dim_red
+        self.n_comp = n_comp
+
+    def preprocess_train(self, X, y):
+        cross_pt_trials = [x.shape[0] for x, _ in self.cross_pt_data]
+        tar_trials = X.shape[0]
+        # reshape features to be 2D (preserve last dimension for reduction)
+        X_cross_r = [x.reshape(-1, x.shape[-1]) for x, _ in self.cross_pt_data]
+        X_tar_r = X.reshape(-1, X.shape[-1])
+        # reduce dimensionality of cross-patient data
+        X_cross_dr = [self.dim_red(n_components=self.n_comp).fit_transform(x)
+                      for x in X_cross_r]
+
+        # reduce dimensionality of target data, saving dim. red. object for 
+        # test set
+        tar_dr = self.dim_red(n_components=self.n_comp)
+        X_tar_dr = tar_dr.fit_transform(X_tar_r)
+        self.tar_dr = tar_dr
+
+        # reshape back to 3D
+        X_cross_dr = [x.reshape(cross_pt_trials[i], -1, x.shape[-1]) for i, x
+                      in enumerate(X_cross_dr)]
+        X_cross_dr = [x.reshape(x.shape[0], -1) for x in X_cross_dr]
+        X_tar_dr = X_tar_dr.reshape(X.shape[0], -1)
+
+        # concatenate cross-patient data
+        X_dr = np.vstack([X_tar_dr] + X_cross_dr)
+        y_dr = np.hstack([y] + [y for _, y in self.cross_pt_data])
+        return X_dr, y_dr
+    
+    def preprocess_test(self, X):
+        X_r = X.reshape(-1, X.shape[-1])
+        X_dr =  self.tar_dr.transform(X_r)
+        return X_dr.reshape(X.shape[0], -1)
+        
+    def fit(self, X, y):
+        X_dr, y_dr = self.preprocess_train(X, y)
+        return self.decoder.fit(X_dr, y_dr)
+    
+    def predict(self, X):
+        X_dr = self.preprocess_test(X)
+        return self.decoder.predict(X_dr)
+    
+    def score(self, X, y, **kwargs):
+        X_dr = self.preprocess_test(X)
+        return self.decoder.score(X_dr, y, **kwargs)
