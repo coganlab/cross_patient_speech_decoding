c0a9f006e977f8c208abe086ff9086588608318f
Zac Spalding <spaldingzac@gmail.com>
2023-10-30 19:11:36 -0400

    trouble with search cv

--- phoneme_encoder_decoder/alignment/cross_pt_decoders.py
+++ phoneme_encoder_decoder/alignment/cross_pt_decoders.py
@@ -7,6 +7,7 @@ import numpy as np
 from sklearn.base import BaseEstimator
 from sklearn.decomposition import PCA
 
+
 class crossPtDecoder(BaseEstimator):
 
     def preprocess_train(self, X, y=None):
@@ -15,21 +16,22 @@ class crossPtDecoder(BaseEstimator):
     def preprocess_test(self, X, y=None):
         pass
 
-    def fit(self, X, y):
-        X_p, y_p = self.preprocess_train(X, y)
+    def fit(self, X, y, **kwargs):
+        X_p, y_p = self.preprocess_train(X, y, **kwargs)
         return self.decoder.fit(X_p, y_p)
-    
+
     def predict(self, X):
         X_p = self.preprocess_test(X)
         return self.decoder.predict(X_p)
-    
+
     def score(self, X, y, **kwargs):
         X_p = self.preprocess_test(X)
         return self.decoder.score(X_p, y, **kwargs)
 
+
 class crossPtDecoder_sepDimRed(crossPtDecoder):
     """ Cross-Patient Decoder with separate PCA for each patient. """
-    
+
     def __init__(self, cross_pt_data, decoder, dim_red=PCA, n_comp=10):
         self.cross_pt_data = cross_pt_data
         self.decoder = decoder
@@ -40,13 +42,14 @@ class crossPtDecoder_sepDimRed(crossPtDecoder):
         cross_pt_trials = [x.shape[0] for x, _, _ in self.cross_pt_data]
         tar_trials = X.shape[0]
         # reshape features to be 2D (preserve last dimension for reduction)
-        X_cross_r = [x.reshape(-1, x.shape[-1]) for x, _, _ in self.cross_pt_data]
+        X_cross_r = [x.reshape(-1, x.shape[-1]) for x, _, _ in
+                     self.cross_pt_data]
         X_tar_r = X.reshape(-1, X.shape[-1])
         # reduce dimensionality of cross-patient data
         X_cross_dr = [self.dim_red(n_components=self.n_comp).fit_transform(x)
                       for x in X_cross_r]
 
-        # reduce dimensionality of target data, saving dim. red. object for 
+        # reduce dimensionality of target data, saving dim. red. object for
         # test set
         tar_dr = self.dim_red(n_components=self.n_comp)
         X_tar_dr = tar_dr.fit_transform(X_tar_r)
@@ -62,12 +65,12 @@ class crossPtDecoder_sepDimRed(crossPtDecoder):
         X_dr = np.vstack([X_tar_dr] + X_cross_dr)
         y_dr = np.hstack([y] + [y for _, y, _ in self.cross_pt_data])
         return X_dr, y_dr
-    
+
     def preprocess_test(self, X):
         X_r = X.reshape(-1, X.shape[-1])
-        X_dr =  self.tar_dr.transform(X_r)
+        X_dr = self.tar_dr.transform(X_r)
         return X_dr.reshape(X.shape[0], -1)
-    
+
 
 class crossPtDecoder_sepAlign(crossPtDecoder):
 
@@ -83,13 +86,14 @@ class crossPtDecoder_sepAlign(crossPtDecoder):
         cross_pt_trials = [x.shape[0] for x, _, _ in self.cross_pt_data]
         tar_trials = X.shape[0]
         # reshape features to be 2D (preserve last dimension for reduction)
-        X_cross_r = [x.reshape(-1, x.shape[-1]) for x, _, _ in self.cross_pt_data]
+        X_cross_r = [x.reshape(-1, x.shape[-1]) for x, _, _ in
+                     self.cross_pt_data]
         X_tar_r = X.reshape(-1, X.shape[-1])
         # reduce dimensionality of cross-patient data
         X_cross_dr = [self.dim_red(n_components=self.n_comp).fit_transform(x)
                       for x in X_cross_r]
 
-        # reduce dimensionality of target data, saving dim. red. object for 
+        # reduce dimensionality of target data, saving dim. red. object for
         # test set
         tar_dr = self.dim_red(n_components=self.n_comp)
         X_tar_dr = tar_dr.fit_transform(X_tar_r)
@@ -110,7 +114,7 @@ class crossPtDecoder_sepAlign(crossPtDecoder):
         for i, algn in enumerate(algns):
             algn.fit(X_tar_dr, X_cross_dr[i], y_align, y_align_cross[i])
             X_algn_dr.append(algn.transform(X_cross_dr[i]))
-        
+
         X_algn_dr = [x.reshape(x.shape[0], -1) for x in X_algn_dr]
         X_tar_dr = X_tar_dr.reshape(X_tar_dr.shape[0], -1)
 
@@ -118,10 +122,8 @@ class crossPtDecoder_sepAlign(crossPtDecoder):
         X_pool = np.vstack([X_tar_dr] + X_algn_dr)
         y_pool = np.hstack([y] + [y for _, y, _ in self.cross_pt_data])
         return X_pool, y_pool
-    
+
     def preprocess_test(self, X):
         X_r = X.reshape(-1, X.shape[-1])
-        X_dr =  self.tar_dr.transform(X_r)
+        X_dr = self.tar_dr.transform(X_r)
         return X_dr.reshape(X.shape[0], -1)
-
-
