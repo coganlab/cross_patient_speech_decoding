9d82700190cd8d81b861f241268ae01d3f233e5a
Zac Spalding <spaldingzac@gmail.com>
2024-09-13 08:21:43 -0400

    updates to training code

--- aligned_decoding/transformers/train.py
+++ aligned_decoding/transformers/train.py
@@ -1,15 +1,21 @@
 # %% Imports
-import torch
+# %% Imports
 import lightning as L
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks.model_checkpoint import ModelCheckpoint
 from pytorch_lightning.utilities.model_summary import summarize
-from datamodules import SimpleMicroDataModule, CCAMicroDataModule
+import torch
+from datamodules import SimpleMicroDataModule, AlignedMicroDataModule
 from models import CNNTransformer, Transformer
+import augmentations as augs
+
 import os
 import sys
 sys.path.append('..')
 from alignment import alignment_utils as utils
+from alignment.AlignCCA import AlignCCA
+
+
 import warnings
 warnings.filterwarnings("ignore", ".*does not have many workers.*")
 warnings.filterwarnings("ignore", "The number of training batches.*")
@@ -18,10 +24,8 @@ print('Done imports')
 # %% Define data module
 
 # data_filename = '../data/pt_decoding_data_S62.pkl'
-data_path = os.path.expanduser('~') + '/data/'
-# data_path = '../data/'
-data_filename = 'pt_decoding_data_S62.pkl'
-pt_data = utils.load_pkl(data_path + data_filename)
+data_filename = os.path.expanduser('~/data/pt_decoding_data_S62.pkl')
+pt_data = utils.load_pkl(data_filename)
 
 pt = 'S14'
 p_ind = -1
@@ -38,6 +42,7 @@ print([[d.shape for d in p] for p in pre_data])
 # n_timepoints = 200
 # n_features = 111
 fs = 200 # Hz
+augmentations = [augs.time_warping, augs.time_masking, augs.time_shifting, augs.noise_jitter, augs.scaling]
 # data = torch.rand(n_samples, n_timepoints, n_features)
 # labels = torch.randint(0, 9, (n_samples,))
 # data = torch.Tensor(all_pt_dict['S14']['X1'])
@@ -54,12 +59,13 @@ batch_size = -1
 n_folds = 20
 val_size = 0.1
 # dm = SimpleMicroDataModule(data, labels, batch_size=batch_size, folds=n_folds,
-#                            val_size=val_size)
-dm = CCAMicroDataModule(data, labels, align_labels, pool_data, batch_size=batch_size, folds=n_folds, val_size=val_size)
-print('Done imports')
+#                            val_size=val_size, augmentations=augmentations)
+dm = AlignedMicroDataModule(data, labels, align_labels, pool_data, AlignCCA,
+                            batch_size=batch_size, folds=n_folds, val_size=val_size,
+                            augmentations=augmentations)
+# dm.setup()
 
-
-# %% Create model
+# %% Define model
 
 # model parameters
 in_channels = data.shape[-1]
@@ -82,49 +88,50 @@ gclip_val = 0.5
 sum_model = CNNTransformer(in_channels, num_classes, d_model, kernel_size, stride, padding,
                            n_head, num_layers, dim_fc, dropout, learning_rate)
 print(summarize(sum_model))
-# sum_model = Transformer(in_channels, num_classes, d_model, kernel_size, stride, padding,
-#                            n_head, num_layers, dim_fc, dropout, learning_rate)
-# print(summarize(sum_model))
 
-# %% Train the model
+
+# %% Train model
 
 # instantiate the trainer
 max_epochs = 500
 es_pat = max_epochs // 20
 # callbacks = [EarlyStopping(monitor='val_loss', patience=10)]
 
-
 # train the model
-dm.setup()
-
-fold_accs = []
-# for fold in range(n_folds):
-for fold in range(1):
-    dm.set_fold(fold)
-    # print(dm.current_fold)
+n_iters = 1
+iter_accs = []
+for i in range(n_iters):
+    dm.setup()
     
-    # instantiate the model
-    in_channels = dm.get_data_shape()[-1]
-    model = CNNTransformer(in_channels, num_classes, d_model, kernel_size, stride, padding,
-                           n_head, num_layers, dim_fc, dropout, learning_rate)
+    fold_accs = []
+    for fold in range(n_folds):
+        dm.set_fold(fold)
+        # print(dm.current_fold)
+        
+        # instantiate the model
+        in_channels = dm.get_data_shape()[-1]
+        model = CNNTransformer(in_channels, num_classes, d_model, kernel_size, stride, padding,
+                               n_head, num_layers, dim_fc, dropout, learning_rate)
+        
+        # model.current_fold = fold
+        callbacks = [ModelCheckpoint(monitor='val_loss'), EarlyStopping(monitor='val_loss', patience=es_pat)]
+        trainer = L.Trainer(max_epochs=max_epochs,
+                            gradient_clip_val=gclip_val,
+                            accelerator='auto',
+                            callbacks=callbacks,
+                            logger=True,
+                            enable_model_summary=False,
+                            enable_progress_bar=False,
+                           )
+        trainer.fit(model, dm)
+        print(trainer.logged_metrics)
+        trainer.test(model, dm)
+        fold_accs.append(trainer.logged_metrics['test_acc'])
     
-    # model.current_fold = fold
-    callbacks = [ModelCheckpoint(monitor='val_loss'), EarlyStopping(monitor='val_loss', patience=es_pat)]
-    trainer = L.Trainer(max_epochs=max_epochs,
-                        gradient_clip_val=gclip_val,
-                        accelerator='auto',
-                        callbacks=callbacks,
-                        logger=True,
-                        enable_model_summary=False,
-                        # enable_progress_bar=False,
-                       )
-    trainer.fit(model, dm)
-    print(trainer.logged_metrics)
-    trainer.test(model, dm)
-    fold_accs.append(trainer.logged_metrics['test_acc'])
-
-    # save loss information
-    # loss_dict = trainer.logger.metrics
-    # loss_dict['fold'] = fold
-    # loss_dict['model'] = model
-print(f'Averaged accuracy: {sum(fold_accs) / len(fold_accs)}')
+        # save loss information
+        # loss_dict = trainer.logger.metrics
+        # loss_dict['fold'] = fold
+        # loss_dict['model'] = model
+    print(f'Averaged accuracy: {sum(fold_accs) / len(fold_accs)}')
+    iter_accs.append(fold_accs)
+print(sum(iter_accs) / len(iter_accs), iter_accs)
