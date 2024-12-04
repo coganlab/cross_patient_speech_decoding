03f24688148dde989b22d10ec931b319a89f4b63
Zac Spalding <spaldingzac@gmail.com>
2024-12-04 11:05:16 -0500

    added function to get coordinates for spatial average grids that contain significant channels by patient

--- aligned_decoding/processing_utils/spatial_avg_subsampling.py
+++ aligned_decoding/processing_utils/spatial_avg_subsampling.py
@@ -1,7 +1,51 @@
 import numpy as np
+import scipy.io as sio
 import matplotlib.pyplot as plt
 
 from grid_subsampling import grid_susbsample_idxs
+
+
+def spatial_avg_sig_channels(pt, contactSize, dataPath):
+    # load in channel map
+    chanMap = sio.loadmat(f'{dataPath}/{pt}/{pt}_channelMap.mat')['chanMap']
+
+    # load in significant channel data
+    sigChan = np.squeeze(
+        sio.loadmat(f'{dataPath}/{pt}/{pt}_sigChannel.mat')['sigChannel'])
+
+    # trim full nan edges if necessary
+    if chanMap.shape[1] == 24:
+        chanMap = chanMap[:,1:-1]
+
+    # get spatial average indices
+    gridSize = chanMap.shape
+    avgIdxs = spatial_avg_idxs(gridSize, contactSize)
+
+
+    sigIdxList = []
+    for idxs in avgIdxs:
+        elecPt = chanMap[idxs[:, 0], idxs[:, 1]]
+        
+        # don't consider sets with over half nans
+        if np.sum(np.isnan(elecPt)) >= len(elecPt) / 2:
+            continue
+        else: # remove nans
+            goodIdxs = ~np.isnan(elecPt)
+            elecPt = elecPt[goodIdxs].astype(int)
+            idxsPt = idxs[goodIdxs]
+
+        # see if any significant channels are in the subsampled set
+        commonElec = np.intersect1d(sigChan, elecPt)
+
+        # only save if we sample at least 1 significant channel
+        if commonElec.size > 0:
+            sigIdxList.append(idxsPt)
+    
+    # return channels in coodinate form since the averaging may contain non-
+    # significant channels and we need to access grid form of data for non-
+    # significant channels
+    return sigIdxList
+
 
 def spatial_avg_idxs(gridSize, contactSize):
     # contacts to average are square subsampled grids, so we re-use the grid
@@ -18,9 +62,9 @@ def spatial_avg_idxs(gridSize, contactSize):
 
     # gridIdxs is a list where each element is a list of 2d indices for
     # channels that will should be averaged together to a single channel
-    gridIdxs = grid_susbsample_idxs(gridSize, winSize, step, start)
+    avgIdxs = grid_susbsample_idxs(gridSize, winSize, step, start)
 
-    return gridIdxs
+    return avgIdxs
 
 
 if __name__ == '__main__':
